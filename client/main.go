e33858742136c9d5b0347a3cd1008b52f760f7ad
nyuusen <pppike.0131@gmail.com>
2024-12-25 17:28:48 +0900

    client: 送信先ポート番号をコマンド引数で指定できるように改良
    - リクエスト内容をハードコーディングする形に修正(元の形だとApacheサーバーからエラーが返却されてしまうため)

--- client/main.go
+++ client/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 	"os"
@@ -9,11 +10,16 @@ import (
 func main() {
 	fmt.Println("client start.")
 
-	localSrvPort := ":8080"
+	port := flag.Int("port", 80, "destination server port(int)")
+	flag.Parse()
+
+	fmtPort := fmt.Sprintf(":%d", *port)
+	fmt.Printf("destination server port is %s\n", fmtPort)
+
 	// 指定されたアドレスをTCP通信で利用できる形式に解決する
 	// TCP通信にはソケットが必要で、そのソケットを作成するには接続先のIPとポートが必要となる
 	// IPv4を使うのでtcp4を指定する
-	tcpAddr, err := net.ResolveTCPAddr("tcp4", localSrvPort)
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", fmtPort)
 	errHandler(err, "resolve tcp addr")
 
 	// サーバーにTCP接続を確立する
@@ -25,15 +31,19 @@ func main() {
 
 	// ファイルからリクエスト内容を読み込む
 	// ファイルパスはgoコマンドを実行するディレクトリからの相対パスになる
-	f, err := os.Open("./client/client-send.data")
-	errHandler(err, "open client-send.data")
+	// NOTE: この実装では、Apacheにリクエストを送ることができないため、コメントアウトしている
+	// 発生するエラー：「Your browser sent a request that this server could not understand.」
+	// f, err := os.Open("./client/client_send.data")
+	// errHandler(err, "open client_send.data")
+	// data := make([]byte, 1000)
+	// cnt, err := f.Read(data)
+	// fmt.Printf("read %d byte\n", cnt)
 
-	data := make([]byte, 1000)
-	cnt, err := f.Read(data)
-	fmt.Printf("read %d byte", cnt)
+	req := "GET / HTTP/1.1\r\nHost: localhost\r\n\r\n"
+	reqBytes := []byte(req)
 
 	// ソケットにリクエストデータを書き込む
-	_, err = conn.Write(data)
+	_, err = conn.Write(reqBytes)
 	errHandler(err, "write to socket")
 
 	// ソケットからレスポンスデータを読み込む
@@ -42,12 +52,12 @@ func main() {
 	errHandler(err, "read from socket")
 
 	// レスポンスデータをファイルに書き込む
-	f2, err := os.Create("./client/client-received.data")
-	errHandler(err, "open client-received.data")
+	f2, err := os.Create("./client/client_recv.data")
+	errHandler(err, "open client_recv.data")
 
 	_, err = f2.Write(res)
 
-	fmt.Println("response: ", string(res[:len]))
+	fmt.Printf("response: %s\n", string(res[:len]))
 
 	// 接続を切断
 	conn.Close()
